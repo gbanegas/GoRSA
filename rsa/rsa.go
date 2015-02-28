8e4049902c509c9a86872733f510ee11944e1137
Vader@sith <gustavosouzabanegas@gmail.com>
2015-02-28 16:13:50 -0300

    ad

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -43,16 +43,24 @@ func GenerateKeys() (*PrivateKey, *PublicKey)  {
     pi := big.NewInt(pn[randInt(len(pn)/2, len(pn))])
     qi := big.NewInt(pn[randInt(len(pn)/2, len(pn))])
     di := big.NewInt(0)
-    pe := pi.Sub(pi, big.NewInt(1))
-    qe := qi.Sub(qi, big.NewInt(1))
+    qe := big.NewInt(0)
+    pe := big.NewInt(0)
+
+    pe.SetBytes(pi.Bytes())
+    qe.SetBytes(qi.Bytes())
+
+    totient := big.NewInt(0)
+    pe.Sub(pe, big.NewInt(1))
+    qe.Sub(qe, big.NewInt(1))
     fmt.Println("pe = ", pe)
     fmt.Println("qe = ", qe)
     fmt.Println("pi = ", pi)
     fmt.Println("qi = ", qi)
-    totient := pi.Mul(pe, qe)
+    totient.Mul(pe, qe)
     fmt.Println("tot = ", totient)
     ni := pi.Mul(pi, qi)
-    ei := big.NewInt(0)
+    ei := big.NewInt(randInt(1, totient))
+    
     fmt.Println("pi = ", pi)
     return &PrivateKey{p: pi, q: qi, d : di,n : ni}, &PublicKey{n : ni, e : ei}
     
