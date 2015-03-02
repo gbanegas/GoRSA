b049ef7e6fe17b272d88d64b4e8861b9cecaf045
Vader@sith <gustavosouzabanegas@gmail.com>
2015-03-01 21:00:09 -0300

    add

--- main.go
+++ main.go
@@ -12,14 +12,13 @@ func main(){
 
     fmt.Println("priv = ", priv)
     fmt.Println("pub = ", pub)
-    sample := big.NewInt(1)
+    sample := big.NewInt(1234)
 
     ciphered := rsa.Cipher(pub,sample.Bytes())
-
     fmt.Println(ciphered)
-    ciphered_bytes := []byte(ciphered)
-
-    result := rsa.Decipher(priv, ciphered_bytes)
-    fmt.Println("result = ", result)
+    result := rsa.Decipher(priv, ciphered)
+    c := new(big.Int)
+    c.SetBytes(result)
+    fmt.Println("result = ", c)
 
 }
